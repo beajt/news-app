5a4857b9180b02db116f318a938f589e4b2bdc07
beatuano <beatuano@gmail.com>
2023-10-09 18:53:27 -0400

    news component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>AI Access Almanac</h1>
+      <h2>Read all things AI - filtered by popular articles</h2>
+    </div>
+  );
+}
+
+export default Header
